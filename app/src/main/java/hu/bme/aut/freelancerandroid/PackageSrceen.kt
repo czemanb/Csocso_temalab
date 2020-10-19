275d0a237e5d583f5e68c9f1087de4df1a94e8b8
VaradiTamas <61427905+VaradiTamas@users.noreply.github.com>
2020-10-19 11:46:23 +0200

    main_sreen
    main screen ready

---
+++ app/src/main/java/hu/bme/aut/freelancerandroid/PackageSrceen.kt
@@ -0,0 +1,11 @@
+package hu.bme.aut.freelancerandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PackageSrceen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_package_sreen)
+    }
+}
---
+++ app/src/main/java/hu/bme/aut/freelancerandroid/TransportScreen.kt
@@ -0,0 +1,11 @@
+package hu.bme.aut.freelancerandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TransportScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_transport_sreen)
+    }
+}
