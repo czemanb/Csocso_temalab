1721fa75e7408043f4ca83ed450dbec00bdcd2ad
czemanb <czemanb@gmail.com>
2020-11-02 11:01:25 +0100

    add RegisterCar,RegisterTransport

---
+++ app/src/main/java/hu/bme/aut/freelancerandroid/fragments/RegisterCar.kt
@@ -0,0 +1,25 @@
+package hu.bme.aut.freelancerandroid.fragments
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import hu.bme.aut.freelancerandroid.R
+import kotlinx.android.synthetic.main.register_form.*
+
+class RegisterCar : Fragment(R.layout.register_car), View.OnClickListener {
+    lateinit var navController: NavController
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
+
+    }
+
+    override fun onClick(v: View?) {
+        when (v!!.id) {
+        
+        }
+    }
+}
