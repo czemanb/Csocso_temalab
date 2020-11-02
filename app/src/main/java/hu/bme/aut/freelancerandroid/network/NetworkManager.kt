c57a02f26a76aa519e1b1a5022ab60491a940d4e
czemanb <czemanb@gmail.com>
2020-11-02 13:57:43 +0100

    add endpoints to FreelancerApi

--- app/src/main/java/hu/bme/aut/freelancerandroid/network/NetworkManager.kt
+++ app/src/main/java/hu/bme/aut/freelancerandroid/network/NetworkManager.kt
@@ -5,7 +5,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object NetworkManager {
-    private const val SERVICE_URL = "" // ToDo
+    private const val SERVICE_URL = "localhost:8080/api"
 
     private val freelancerApi: FreelancerApi
 
