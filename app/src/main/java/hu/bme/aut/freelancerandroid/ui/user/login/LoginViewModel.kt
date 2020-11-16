dc1fcc1562b77e893358a13d8707e2e174c075eb
czemanb <czemanb@gmail.com>
2020-11-16 11:17:07 +0100

    fix FreelancerApi, add LoginViewModel

--- app/src/main/java/hu/bme/aut/freelancerandroid/ui/user/login/LoginViewModel.kt
+++ app/src/main/java/hu/bme/aut/freelancerandroid/ui/user/login/LoginViewModel.kt
@@ -1,11 +1,38 @@
 package hu.bme.aut.freelancerandroid.ui.user.login
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.SavedStateHandle
-import androidx.lifecycle.ViewModel
+import android.view.View
+import androidx.lifecycle.*
+import hu.bme.aut.freelancerandroid.repository.model.LoginResponse
 import hu.bme.aut.freelancerandroid.repository.model.User
+import hu.bme.aut.freelancerandroid.repository.repo.user.UserRepository
+import hu.bme.aut.freelancerandroid.util.Resource
+import kotlinx.coroutines.launch
+import retrofit2.Response
 
-class LoginViewModel(savedStateHandle: SavedStateHandle): ViewModel() {
-    val userId : String = savedStateHandle["uid"] ?: throw IllegalArgumentException("missing user id")
-    val user : LiveData<User> = TODO()
+class LoginViewModel(val userRepository: UserRepository): ViewModel() {
+
+    lateinit var email: String
+    lateinit var password: String
+    val activeUser: MutableLiveData<Resource<LoginResponse>> = MutableLiveData()
+
+
+
+    fun loginUser(email: String, password:String) = viewModelScope.launch {
+        activeUser.postValue(Resource.Loading())
+        val response = userRepository.loginUser(email, password)
+        activeUser.postValue(handleLoginResponse(response))
+    }
+
+    private fun handleLoginResponse(response: Response<LoginResponse>) : Resource<LoginResponse> {
+        if(response.isSuccessful) {
+            response.body()?.let { resultResponse ->
+                return Resource.Success(resultResponse)
+            }
+        }
+        return Resource.Error(response.message())
+    }
+
+
+
+
 }
---
+++ app/src/main/java/hu/bme/aut/freelancerandroid/ui/user/register/RegisterViewModel.kt
@@ -0,0 +1,7 @@
+package hu.bme.aut.freelancerandroid.ui.user.register
+
+import androidx.lifecycle.ViewModel
+
+class RegisterViewModel(): ViewModel() {
+
+}
